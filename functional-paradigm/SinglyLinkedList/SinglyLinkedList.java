1559480880e79ec8a6d644b3d9661f521c949031
ReidoBoss <bryansagarino222@gmail.com>
2025-04-16 20:23:23 +0800

    Update SinglyLinkedList.java

--- functional-paradigm/SinglyLinkedList/SinglyLinkedList.java
+++ functional-paradigm/SinglyLinkedList/SinglyLinkedList.java
@@ -1,65 +1,16 @@
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class SinglyLinkedList<T> {
-  private T data;
-  private SinglyLinkedList<T> next;
+  final T data;
+  final SinglyLinkedList<T> next;
+  private final boolean isDataNull;
+  private final boolean isNextNull;
 
-  private SinglyLinkedList(T data) {
+  private SinglyLinkedList(final T data, final SinglyLinkedList<T> next) {
     this.data = data;
-    this.next = null;
+    this.next = next;
+    this.isDataNull = data == null;
+    this.isNextNull = next == null;
   }
-
-  public static <T> SinglyLinkedList<T> empty() {
-    return new SinglyLinkedList<>(null);
-  }
-
-  public <R> SinglyLinkedList<R> map(Function<T, R> transform) {
-    if (this.data == null) {
-      return new SinglyLinkedList<>(null);
-    }
-    SinglyLinkedList<R> newHead = new SinglyLinkedList<>(transform.apply(this.data));
-    SinglyLinkedList<R> currentNew = newHead;
-    SinglyLinkedList<T> currentOld = this.next;
-
-    while (currentOld != null && currentOld.data != null) {
-      currentNew.next = new SinglyLinkedList<>(transform.apply(currentOld.data));
-      currentNew = currentNew.next;
-      currentOld = currentOld.next;
-    }
-
-    return newHead;
-  }
-
-  public SinglyLinkedList<T> insert(T e) {
-    if (this.data == null) {
-      return new SinglyLinkedList<T>(e);
-    } else if (this.next == null) {
-      SinglyLinkedList<T> result = new SinglyLinkedList<>(this.data);
-      result.next = new SinglyLinkedList<>(e);
-      return result;
-    } else {
-      SinglyLinkedList<T> result = new SinglyLinkedList<>(this.data);
-      result.next = this.next.insert(e);
-      return result;
-    }
-  }
-
-  @Override
-  public String toString() {
-
-    StringBuilder sb = new StringBuilder("SinglyLinkedList(");
-    SinglyLinkedList<T> current = this;
-
-    while (current != null && current.data != null) {
-      sb.append(current.data);
-      current = current.next;
-      if (current != null && current.data != null) {
-        sb.append(", ");
-      }
-    }
-
-    sb.append(")");
-    return sb.toString();
-  }
-
 }
